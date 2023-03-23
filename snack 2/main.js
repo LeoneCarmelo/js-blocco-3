7a3aa0f934314d62bbdf087d4ca80d4a69a99dfb
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-23 12:22:48 +0100

    Add code Snack 2

---
+++ snack 2/main.js
@@ -0,0 +1,10 @@
+/* A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+const characters = ['pippo', 'PLUTO', 'Paperino']
+
+characters.forEach((character) => {
+    const char = character.toLowerCase().charAt(0).toUpperCase() + character.slice(1)
+    console.log(char)
+})
